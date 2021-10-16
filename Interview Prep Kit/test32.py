3913e3e6eb4a7e23de80060e18b743c79c6b5b99
Op-Design <o.immanuel.o@gmail.com>
2021-10-16 12:25:28 -0500

    Added gitignore
    Added some updates

---
+++ Interview Prep Kit/test32.py
@@ -0,0 +1,13 @@
+def sockMerchant(ar):
+    # 1. Identify all possible colors
+    sorted_list=[]
+    for index in range(len(list)-1):
+        x = min(ar [index:])
+        sorted_list.append(x)
+    print (sorted_list)
+
+    
+
+
+ar = [10,12,15,30, 16, 13, 12]
+sockMerchant(ar)
