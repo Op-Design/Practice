ea12563932d8d03f5442466ee549cf2e0c704e08
Op-Design <o.immanuel.o@gmail.com>
2021-10-28 19:08:03 -0500

    update

--- Interview Prep Kit/String Manipulation/sherlock_and_the_valid_string.py
+++ Interview Prep Kit/String Manipulation/sherlock_and_the_valid_string.py
@@ -1,6 +1,19 @@
-test = {
-    "a": 12,
-    "b": 16,
-    }
+# Solution
+# Got stuck on my solution
 
-print (test(1))
+# from collections import Counter
+
+# def isValid(s):
+#     d = Counter(Counter(s).values())
+#     if len(d)==1:
+#         return "YES"
+#     if len(d)>2:
+#         return "NO"
+#     if 1 in d.values() and (d[min(d.keys())]==1 or (max(d.keys()) - min(d.keys())==1)):
+#         return "YES"
+#     else:
+#         return "NO"
+
+# s = "aaabbbcccd"
+
+# print (isValid(s))
