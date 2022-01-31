5a4a075e1b03eaaf4de068746496cee5a1f302ea
Op-Design <o.immanuel.o@gmail.com>
2022-01-31 14:50:34 -0500

    Pynative convert_to_json.py solved

---
+++ Pynative/JSON/convert_to_json.py
@@ -0,0 +1,17 @@
+import json
+
+"""
+Practice Problem: https://pynative.com/python-json-exercise/#h-exercise-1-convert-the-following-dictionary-into-json-format
+Exercise 1: Convert the following dictionary into JSON format
+data = {"key1" : "value1", "key2" : "value2"}
+"""
+
+
+
+print ("\n")
+data = {"key1" : "value1", "key2" : "value2"}
+print (data)
+print ("\n")
+
+data_json = json.dumps(data, indent=4)
+print (data_json)
